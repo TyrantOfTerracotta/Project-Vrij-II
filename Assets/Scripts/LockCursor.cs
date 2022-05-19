71f2af918705e8513d1f7ff11be14258f7b3dcf5
TyrantOfTerracotta <66208966+TyrantOfTerracotta@users.noreply.github.com>
2022-05-19 20:57:30 +0200

    Zooi

---
+++ Assets/Scripts/LockCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void OnApplicationFocus(bool ApplicationIsBack)
+    {
+        if (ApplicationIsBack == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
---
+++ Assets/Scripts/keyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class keyScript : MonoBehaviour
+{
+    [SerializeField] public GameObject barrier;
+    [SerializeField] public GameObject gateOrb;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Key")
+        {
+            //FindObjectOfType<AudioManager>().Play("KeyFound");
+            Debug.Log("The door got unlocked!");
+            Destroy(barrier.gameObject);
+            //Destroy(gateOrb.gameObject);
+            //Destroy(this.gameObject);
+        }
+    }
+}
